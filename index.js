35b00243aaaab80e203ccecae4b49216d60840a7
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-02-23 20:59:58 +0100

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+var expect = require('expect');
+var Redux = require('redux');
+var createStore = Redux.createStore;
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+expect(
+  counter(0, { type: 'INCREMENT' })
+).toEqual(1);
+
+expect(
+  counter(1, { type: 'INCREMENT' })
+).toEqual(2);
+
+console.log("test passed!")
+
