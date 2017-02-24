3e6090d1da2534b02116863205937f382b5eb977
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-02-24 18:14:10 +0100

    finish step 8. add react to display state and buttons to change state

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var redux = require('redux');
+var reactDOM = require('react-dom');
+var React = require('react');
+
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -9,45 +13,39 @@ const counter = (state = 0, action) => {
   }
 }
 
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
-
-  const getState = () => {
-    return state;
-  }
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
-  }
-
-  const subscribe = (listener) => {
-    listeners.push(listener);
-  }
-
-  dispatch({})
-
-  return { getState, dispatch, subscribe };
-}
-
+const createStore = redux.createStore;
 const store = createStore(counter);
 
+class Counter extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.value}</h1>
+        <button onClick={this.props.onDecrementClick}>-</button>
+        <button onClick={this.props.onIncrementClick}>+</button>
+      </div>
+    )
+  }
+}
 
 store.dispatch({ type: 'INCREMENT' });
 
 
 
 const render = () => {
-  document.body.innerText = store.getState();
+  reactDOM.render(
+    <Counter 
+      value={store.getState()} 
+      onDecrementClick={() => store.dispatch({type: 'DECREMENT'})}
+      onIncrementClick={() => store.dispatch({type: 'INCREMENT'})}
+    />,
+    document.getElementById('root')
+  );
 }
 
 store.subscribe(() => {
   render();
-})
-render();
+});
 
-document.body.addEventListener('click', (event) => {
-  store.dispatch({ type: 'INCREMENT' });
-})
+render();
 
