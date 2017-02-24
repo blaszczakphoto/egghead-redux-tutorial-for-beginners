c1a85bddc5a954738384c3dda3ffaed13493fc08
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-02-24 18:46:21 +0100

    finish step 8. avoid array mutations

--- index.js
+++ index.js
@@ -1,51 +1,18 @@
-var redux = require('redux');
-var reactDOM = require('react-dom');
-var React = require('react');
+import expect from 'expect'
+import deepFreeze from 'deep-freeze'
 
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
+const addCounter = (list) => {
+  return list.concat([0]);
 }
 
-const createStore = redux.createStore;
-const store = createStore(counter);
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
 
-class Counter extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.value}</h1>
-        <button onClick={this.props.onDecrementClick}>-</button>
-        <button onClick={this.props.onIncrementClick}>+</button>
-      </div>
-    )
-  }
+  deepFreeze(listBefore);
+
+  expect(addCounter(listBefore)).toEqual(listAfter);
 }
 
-store.dispatch({ type: 'INCREMENT' });
-
-
-
-const render = () => {
-  reactDOM.render(
-    <Counter 
-      value={store.getState()} 
-      onDecrementClick={() => store.dispatch({type: 'DECREMENT'})}
-      onIncrementClick={() => store.dispatch({type: 'INCREMENT'})}
-    />,
-    document.getElementById('root')
-  );
-}
-
-store.subscribe(() => {
-  render();
-});
-
-render();
-
+testAddCounter();
+console.log("test passed!");
