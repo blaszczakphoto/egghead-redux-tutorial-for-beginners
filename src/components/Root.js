10ad3201a981392ea88ac9e997ad9359a13bde54
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-03-08 14:01:22 +0100

    lesson 5. add react router to the project

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
 import TodoApp from './TodoApp'
 import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <TodoApp />
+    <Router history={browserHistory}>  
+      <Route path='/' component={TodoApp} />
+    </Router>
   </Provider>
 );
 
