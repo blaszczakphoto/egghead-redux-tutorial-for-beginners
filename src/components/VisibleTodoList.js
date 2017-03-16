c24caeda0762d573caeaf06805cf97b2d77e57ee
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-03-16 14:39:00 +0100

    lesson 10. put all selectors functions in correspopnding reducers, not in components to encapsulate state tree

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -2,23 +2,12 @@ import { connect } from 'react-redux';
 import TodoList from './TodoList';
 import { toggleTodo } from '../actions';
 import { withRouter } from 'react-router';
+import { getVisibleTodos } from '../reducers'
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'all':
-      return todos;
-    case 'completed':
-      return todos.filter((todo) => todo.completed);
-    case 'active':
-      return todos.filter((todo) => !todo.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
-};
 
 const mapStateTodoProps = (state, { params }) => ({
   todos: getVisibleTodos(
-    state.todos,
+    state,
     params.filter || 'all'
   ),
 });
