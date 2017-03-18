4e3b123c54587d009607054793fdfc1715e4c774
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-03-18 10:29:36 +0100

    lesson 17.The Middleware Chain

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,37 +1,35 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-
-  return (action) => {
-    /* eslint-disable no-console */
-    console.group(action.type);
-    console.log('%c prev state', 'color: gray', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = rawDispatch(action);
-    console.log('%c next state', 'color: gray', store.getState());
-    console.groupEnd(action.type);
-    /* eslint-enable no-console */
-    return returnValue;
-  };
+const logger = (store) => (next) => (action) => {
+  /* eslint-disable no-console */
+  console.group(action.type);
+  console.log('%c prev state', 'color: gray', store.getState());
+  console.log('%c action', 'color: blue', action);
+  const returnValue = next(action);
+  console.log('%c next state', 'color: gray', store.getState());
+  console.groupEnd(action.type);
+  /* eslint-enable no-console */
+  return returnValue;
 };
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
-    return rawDispatch(action);
-  };
+const promise = (store) => (next) => (action) => {
+  if (typeof action.then === 'function') {
+    return action.then(next);
+  }
+  return next(action);
+};
+
+const wrapDispatchWithMiddlewares = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware =>
+    store.dispatch = middleware(store)(store.dispatch)
+  );
 };
 
 const configureStore = () => {
   const store = createStore(todoApp);
-
-  store.dispatch = addLoggingToDispatch(store);
-  store.dispatch = addPromiseSupportToDispatch(store);
+  const middlewares = [promise, logger];
+  wrapDispatchWithMiddlewares(store, middlewares);
 
   return store;
 };
